13f9bf2a2f6017f2e33cfbc9b5f4a7f8796617a0
Andres Almiray <aalmiray@gmail.com>
2021-03-01 17:25:11 +0100

    Sort pom

---
+++ sort-pom.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Copyright 2021 Andres Almiray.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+mvn com.github.ekryd.sortpom:sortpom-maven-plugin:sort -Dsort.keepBlankLines -Dsort.sortPlugins=groupId,artifactId -Dsort.sortDependencies=scope,artifactId -Dsort.nrOfIndentSpace=4 -Dsort.sortOrderFile=https://raw.githubusercontent.com/kordamp/kordamp-maven-parent/main/kordamp-sort.xml
+
